470839e93512cff329b33c677361928b8ae40dfa
Arthur Monteiro Alves Melo <monteiroamelo@gmail.com>
2017-11-14 18:15:20 -0300

    metronome_init

--- libs/inc/metronome.h
+++ libs/inc/metronome.h
@@ -1,5 +1,15 @@
 #ifndef METRONOME_H
 
+#include <stdlib.h>
+#include "inc/esp_common.h"
+#include "esp/uart.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "esp8266.h"
+
 #define METRONOME_H
+
+void metronomeTask(void *pvParameters);
+
 
 #endif
--- libs/metronome.c
+++ libs/metronome.c
@@ -1,1 +1,22 @@
+
+/* The classic "blink" example
+ *
+ * This sample code is in the public domain.
+ */
 #include "inc/metronome.h"
+
+const int gpio = 2;
+
+/* This task uses the high level GPIO API (esp_gpio.h) to blink an LED.
+ *
+ */
+void metronomeTask(void *pvParameters)
+{
+    gpio_enable(gpio, GPIO_OUTPUT);
+    while(1) {
+        gpio_write(gpio, 1);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        gpio_write(gpio, 0);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
