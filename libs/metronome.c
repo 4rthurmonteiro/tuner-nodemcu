d7ace941181e488038cfe5bc0cd1785d105a622c
Arthur Monteiro Alves Melo <monteiroamelo@gmail.com>
2017-11-14 23:41:46 -0300

    buzzer_ring_without_pwm

--- libs/metronome.c
+++ libs/metronome.c
@@ -1,18 +1,16 @@
 #include "inc/metronome.h"
 
 const int gpio = 2;
-const int gpio_analog = 0;
 
 void metronomeTask(void *pvParameters)
 {
     gpio_enable(gpio, GPIO_OUTPUT);
-    gpio_enable(gpio_analog, GPIO_INPUT);
     while(1) {
-      short int dutycicle = gpio_analog;
+      short int dutycicle = sdk_system_adc_read();
       if(dutycicle > 1023) dutycicle = 1023;
         gpio_write(gpio, 1);
-        vTaskDelay(1000 / dutycicle);
+        vTaskDelay(dutycycle / portTICK_PERIOD_MS);
         gpio_write(gpio, 0);
-        vTaskDelay(1000 / dutycicle);
+        vTaskDelay(dutycycle / portTICK_PERIOD_MS);
     }
 }
