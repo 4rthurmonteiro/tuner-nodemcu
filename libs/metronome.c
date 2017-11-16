cb2cdfcfad5fe5f04ac7de189d157a609e01603f
Arthur Monteiro Alves Melo <monteiroamelo@gmail.com>
2017-11-15 22:40:36 -0300

    metronome_almost_done

--- libs/metronome.c
+++ libs/metronome.c
@@ -1,16 +1,23 @@
 #include "inc/metronome.h"
 
 const int gpio = 2;
+const int oneMinute = 60;
 
 void metronomeTask(void *pvParameters)
 {
-    gpio_enable(gpio, GPIO_OUTPUT);
+gpio_enable(gpio, GPIO_OUTPUT);
+
+
     while(1) {
-      short int dutycicle = sdk_system_adc_read();
-      if(dutycicle > 1023) dutycicle = 1023;
+        short int dutycycle =  sdk_system_adc_read(); /* read continuous POT and set PWM duty cycle according */
+        if(dutycycle < 60) dutycycle = 61;
+        printf("Variação do Potênciometro: %i\n", dutycycle);
+
         gpio_write(gpio, 1);
-        vTaskDelay(dutycycle / portTICK_PERIOD_MS);
+        vTaskDelay(500 / oneMinute);
+
         gpio_write(gpio, 0);
-        vTaskDelay(dutycycle / portTICK_PERIOD_MS);
+        vTaskDelay((dutycycle / oneMinute)*1000);
+    }
     }
 }
